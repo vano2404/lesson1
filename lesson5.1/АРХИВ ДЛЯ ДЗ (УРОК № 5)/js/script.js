24e02cd99e6798e1a9667bfc1a7af706a594fbbf
vano2404 <49793742+vano2404@users.noreply.github.com>
2019-04-26 15:38:50 +0300

    Обязательное задание №5.1

--- lesson5.1/АРХИВ ДЛЯ ДЗ (УРОК № 5)/js/script.js
+++ lesson5.1/АРХИВ ДЛЯ ДЗ (УРОК № 5)/js/script.js
@@ -1,29 +1,24 @@
 let menu = document.querySelector('.menu'),
-  columns = document.querySelectorAll('.column'),
+  column = document.querySelectorAll('.column'),
   menuItems = document.querySelectorAll('.menu-item'),
-  menuItem = document.createElement('li'),
+  menuItemLi = document.createElement('li'),
   title = document.getElementById('title'),
-  advertising = document.querySelector('.adv'),
-  questionHolder = document.getElementById('prompt'),
-  question = prompt('Как вы относитесь к технике apple ?', '');
+  adv = document.querySelector('.adv');
+  questionPrompt = document.getElementById('prompt'),
+  question = prompt('Как вы относитесь к технике apple ?');
 
-console.log(questionHolder);
-console.log(question);
-
-
-
-menu.appendChild(menuItem);
-
-menuItem.classList.add('menu-item');
-menuItem.textContent = 'Пятый пункт';
-menu.removeChild(menuItems[2]);
+menuItemLi.classList.add('menu-item');
+menu.appendChild(menuItemLi);
+menuItemLi.innerHTML = 'пятый пункт';
 menu.insertBefore(menuItems[2], menuItems[1]);
 
-document.body.style.background = 'url(../img/apple_true.jpg) center no-repeat';
 
-title.textContent = 'Мы продаем только подлинную технику Apple';
-columns[1].removeChild(advertising);
+document.body.style.background = "url('img/apple_true.jpg') center no-repeat"; 
 
-questionHolder.innerHTML = `<h1>${question}</h1>`
+title.textContent = "Мы продаем только подлинную технику Apple";
+
+column[1].removeChild(adv);
+
+questionPrompt.innerHTML = question; 
 
 
