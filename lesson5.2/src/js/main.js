933632650299cd6c676c20fce0c1671a65f5024a
Ivan <vano2404@gmail.com>
2019-04-26 19:27:22 +0300

    Обязательное задание№5.2

---
+++ lesson5.2/src/js/main.js
@@ -0,0 +1,39 @@
+let start = document.getElementById('start'),
+tableResult = document.querySelectorAll('.result-table div'), // панель доход
+itemExpenses = document.getElementsByClassName('expenses-item'), // инпуты с обязательными расходами
+affirm = document.getElementsByTagName('button'), // кнопки  
+itemOptionalexpenses = document.querySelectorAll('.optionalexpenses-item'),// инпунты с необязательными расходами
+labelChooseIncome = document.querySelector('.choose-income-label'),// заголовок выведете статьи
+ChooseIncome = document.querySelector('.choose-income'),  //ввод статей возможного дохода
+checksavings = document.querySelectorAll('.checksavings'), // накопления
+label = document.querySelectorAll('.data label'),// заголовки элементов сумма проценты
+sumPercent = document.querySelector('#sum') // инпут сумма
+percent = document.querySelector('#percent') // инпут процент
+dataClass = document.querySelectorAll('.time-data div'), // год месяц день
+dataInput = document.querySelectorAll('.time-data div input'); // инпуты года даты дня 
+
+console.log(tableResult); // вывод окна доход;
+console.log(itemExpenses); // вывод инпутов  с обязательными расходами
+console.log(affirm); // вывод массива кнопки 
+console.log(itemOptionalexpenses); // вывод инпутов с необязательными расходами
+console.log(labelChooseIncome);// заголовок выведете статьи
+console.log(ChooseIncome); //  инпут для ввода статей возможного дохода
+console.log(checksavings); // накопления
+console.log(label); // вывод элементов с тегом label сумма проценты
+console.log(sum); // инпут сумма
+console.log(percent); // инпут проценты
+console.log(dataClass); // вывод массива с годом месяцем днем 
+console.log(dataInput); // вывод с инпутами
+
+
+
+
+
+
+
+
+
+
+
+
+
